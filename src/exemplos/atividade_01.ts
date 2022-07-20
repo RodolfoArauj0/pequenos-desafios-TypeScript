51a2c0edbef6d184dd47077bad5453142f25a7bf
RodolfoArauj0 <arujo.ro@hotmail.com>
2022-07-20 17:29:17 -0300

    Atividade 01

---
+++ src/exemplos/atividade_01.ts
@@ -0,0 +1,18 @@
+let funcionario = {
+    codigo: 10,
+    nome: "Maria"
+}
+
+let funcionario2: { codigo: number, nome: string } = {
+    codigo: 30,
+    nome: "Maria"
+}
+interface funcionario {
+    codigo: number,
+    nome: string
+}
+
+let funcionario3: funcionario = {
+    codigo: 30,
+    nome: "Maria"
+}
